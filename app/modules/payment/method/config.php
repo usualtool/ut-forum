2881dd267013b62ee9288c1cdc59e9b78b7d5665
黄豆 <292951110@qq.com>
2022-05-26 17:14:24 +0800

    update 220526

--- app/modules/payment/method/config.php
+++ app/modules/payment/method/config.php
@@ -10,26 +10,11 @@
        *  | UT Framework is suitable for Apache2 protocol.  |            
        * --------------------------------------------------------                
 */
-ini_set("error_reporting","E_ALL & ~E_NOTICE");
 /**
- * 开启session
+ * 自动加载容错
  */
-session_start();
-/**
- * 支付模块独立设置
- * 根路径必为UTF_ROOT
- */
-define('UTF_ROOT',dirname(dirname(dirname(dirname(dirname(__FILE__))))));
+require dirname(dirname(dirname(dirname(__FILE__)))).'/'.'config.php';
 /**
  * 支付路径
  */
 define('PAY_PATH',dirname(__FILE__));
-/**
- * 类加载容错
- */
-require UTF_ROOT.'/'.'/library/UsualToolInc.php';
-require UTF_ROOT.'/'.'/library/UsualToolData.php';
-/**
- * 加载配置
- */
-$config=library\UsualToolInc\UTInc::GetConfig();
